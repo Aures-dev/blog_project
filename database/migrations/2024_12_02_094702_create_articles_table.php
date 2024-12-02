29cd1ddb7c0c65027474bbbe60b183c912540890
Aures-dev <auresaz69@gmail.com>
2024-12-02 11:34:06 +0100

    migration pour la table des articles crée

---
+++ database/migrations/2024_12_02_094702_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);//max 255 caractère pour le champ du titre
+            $table->string('image');//champ pour stocker le chemin de l'article
+            $table->text('content')->nullable();//champ pour le contenu de l'article
+            $table->string('file_path')->nullable();//chemin du fichier pdf
+            $table->foreignId('user_id')->nullable();// Crée la colonne 'user_id', qui est une clé étrangère vers la table 'users'
+            $table->timestamps(); // Crée les colonnes 'created_at' et 'updated_at' pour suivre les dates de création et de mise à jour des articles
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');/// Définit la relation étrangère entre 'user_id' et 'id' de la table 'users', et définit la règle de suppression à 'SET NULL'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +20,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        //Article::factory(10)->create();
     }
 }
